d89785b258d4343aa9b3b83b8eeba67a1846fac2
Vladimir Kosteley <zzismd@gmail.com>
2014-03-19 10:12:49 +0700

    Web development, dsvn

--- php-init.el
+++ php-init.el
@@ -4,3 +4,15 @@
 ;(add-to-list 'load-path "~/.emacs.d/packages/php+-mode")
 ;(require 'php+-mode)
 ;(php+-mode-setup)
+
+;-----------------------------------------------------------------
+; nxhtml
+;-----------------------------------------------------------------
+;(load "~/.emacs.d/packages/nxhtml/autostart.el")
+;(eval-after-load 'nxhtml '(nxhtml-toggle-visible-warnings))
+
+;-----------------------------------------------------------------
+; php-mode
+;-----------------------------------------------------------------
+(add-to-list 'load-path "~/.emacs.d/packages/php-mode")
+(require 'php-mode)
